f774bf78d9c66136effb1f3be986639400f19840
Pallishree <pallishreebehera01@gmail.com>
2023-09-12 00:01:30 +0530

    backend api change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = 'http://localhost:8000/dev/api/v1'
+// export const API_URL = 'http://localhost:8000/dev/api/v1'
+export const API_URL = 'https://expensive-mite-underclothes.cyclic.app/dev/api/v1'
 export const CLOUDINARY_NAME = "pallishree - saswat"
 export const CLOUDINARY_KEY = 642596299393146
 export const CLOUDINARY_SECRET = "u2F6NWoqc8oy67FBtz1CmWGJ1EA"
